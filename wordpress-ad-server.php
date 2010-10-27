701df131a02b4c26681f834a4064bc925559e494
Kostas Dizas <kdizas@gmail.com>
2010-10-27 13:39:36 +0100

    added some security

--- wordpress-ad-server.php
+++ wordpress-ad-server.php
@@ -6,10 +6,15 @@
 */
 
 function was_settings() {
+
+	if ( !current_user_can( 'manage_options' ) ) {
+		wp_die( __('You do not have sufficient permissions to access this page.') );
+	}
+
 ?>
 	<div class="wrap">
 		<h2><?php _e('Wordpress Ad Server'); ?></h2>
-		<p>Placeholder</p>
+		<p>Placeholder	</p>
 	</div>
 <?php
 }
