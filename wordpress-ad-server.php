c144667dc6228efb197dd2ead58c19118b6fd959
Kostas Dizas <kdizas@gmail.com>
2010-10-31 02:13:49 +0000

    added a class to help with the database communication. for now it has constructor, add and get functions

--- wordpress-ad-server.php
+++ wordpress-ad-server.php
@@ -6,6 +6,49 @@
 	Author: Kostas Dizas
 */
 
+class WAS_Class {
+
+	/**
+	 * Variables
+	 */
+	$table_name;
+	
+	/**
+	 * Constructor
+	 */
+	function WAS_Class() {
+		global $wpdb;
+		$this->$table_name = $wpdb->prefix . 'was_data';
+	}
+	
+	/**
+	 * Get all entries from the db table
+	 * 
+	 * @return array
+	 */
+	function getEntries() {
+		global $wpdb;
+		$table_name = $wpdb->prefix . 'was_data';
+		$sql = "SELECT * FROM ". $table_name ." ORDER BY `advertisment_id` ASC";
+		$ads = $wpdb->get_results( $sql );
+		return $ads;
+	}
+	
+	/**
+	 * Add an entry to the db table
+	 * 
+	 * @param array $entry
+	 */
+	function addEntry( $entry ) {
+		global $wpdb;
+		
+		$rows_affected = $wpdb->insert( $this->table_name, array(
+			'advertisment_name' => $wpdb->escape( $entry['name'] ),
+			'advertisment_code' => $wpdb->escape( $entry['code'] )
+		));
+	}
+}
+
 /**
  * Settings page
  * 
@@ -20,7 +63,17 @@ function was_settings() {
 ?>
 	<div class="wrap">
 		<h2><?php _e('Wordpress Ad Server'); ?></h2>
-		<p>Placeholder</p>
+		<dl>
+<?php
+	$ads_class = new WAS_Class();
+	foreach( $ads_class->getEntries() as $ad ) {
+?>
+			<dt><?php echo $ad->advertisment_name ?></dt>
+			<dd><textarea><?php echo $ad->advertisment_code ?></textarea></dd>
+<?php
+	}
+?>
+		</dl>
 	</div>
 <?php
 }
@@ -33,6 +86,8 @@ function was_settings() {
  * 
  * @uses shortcode_atts()
  * @todo does nothing right now. 
+ * 
+ * @return string
  */
 function was_shortcode( $atts, $content = null ) {
 	extract( shortcode_atts( array(
@@ -54,15 +109,14 @@ add_shortcode( 'was', 'was_shortcode' );
  */
 function was_install() {
 	global $wpdb;
-	global $was_db_version;
 	$was_db_version = '0.1';
 	
 	$table_name = $wpdb->prefix . 'was_data';
 	
 	if ($wpdb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name) {
-		$sql = "CREATE TABLE `". $table_name ." (
+		$sql = "CREATE TABLE `". $table_name ."` (
 			`advertisment_id` int(11) NOT NULL AUTO_INCREMENT,
-			`advertisment_active` int(11) DEFAULT '' NOT NULL,
+			`advertisment_active` int(11) DEFAULT '1' NOT NULL,
 			`advertisment_name` varchar(200) DEFAULT '' NOT NULL,
 			`advertisment_code` text NOT NULL,
 			PRIMARY KEY  (`advertisment_id`)
