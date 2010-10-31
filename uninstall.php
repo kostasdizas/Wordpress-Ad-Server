78c5a0157600d2734f5f1dc005c69ee4e3dbd5b1
Kostas Dizas <kdizas@gmail.com>
2010-10-31 02:15:10 +0000

    plugin uninstall functions. for now it only deletes the database version number from the system

---
+++ uninstall.php
@@ -0,0 +1,15 @@
+<?php
+
+if( !defined( 'ABSPATH') && !defined('WP_UNINSTALL_PLUGIN') )
+	exit();
+
+if ( function_exists('register_uninstall_hook') )
+	register_uninstall_hook( __FILE__, 'was_uninstall_hook' );
+
+function my_uninstall_hook() {
+	
+	delete_option('was_db_version');
+	
+}
+
+?>
