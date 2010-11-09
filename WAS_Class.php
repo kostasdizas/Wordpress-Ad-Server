b6c90918244d92cae80230c0a20da703d05a3431
kdizas@gmail.com <kdizas@gmail.com>
2010-11-09 03:36:12 +0000

    added functionality to getEntries(). new class function getRandomEntry() uses an algorithm to return a weighted random selection (according to the `_weight` value

--- WAS_Class.php
+++ WAS_Class.php
@@ -18,18 +18,64 @@ class WAS_Class {
 	/**
 	 * Get all entries from the db table
 	 * 
-	 * @return array
+	 * 
+	 * @param string $state     (all|active|inactive)
+	 * @param string $method    (object|count)
+	 * @return array|int
 	 */
-	function getEntries() {
+	function getEntries( $state = 'all', $method = 'object' ) {
 		global $wpdb;
+		if ( $state == 'active' ) {
+			$where = ' WHERE `advertisment_active` = 1';
+		} elseif ( $state == 'inactive' ) {
+			$where = ' WHERE `advertisment_active` = 0';
+		} elseif ( $state == 'all' ) {
+			$where = '';
+		}
 		$sql = "SELECT `advertisment_id` FROM `". $this->table_name ."` ORDER BY `advertisment_id` ASC";
 		$ads = $wpdb->get_results( $sql );
 		
-		foreach( $ads as &$ad ) {
-			$ad = new Advertisment( $ad->advertisment_id );
+		if ( $method == 'object' ) {
+			foreach( $ads as &$ad ) {
+				$ad = new Advertisment( $ad->advertisment_id );
+			}
+			return $ads;
+		} elseif ( $method == 'count' ) {
+			return count( $ads );
+		} else {
+			return false;
 		}
 		
-		return $ads;
+	}
+	
+	/**
+	 * Get a random entry
+	 * 
+	 * @param bool $weighted
+	 * @return Advertisment
+	 */
+	function getRandomEntry( $weighted = true ) {
+		global $wpdb;
+		
+		$sql = "SELECT `advertisment_id`, `advertisment_weight`
+		FROM `". $this->table_name ."`
+		WHERE `advertisment_active` = 1";
+		
+		$ads = $wpdb->get_results( $sql );
+		$sum = 0;
+		foreach( $ads as $ad ) {
+			$sum += $ad->advertisment_weight;
+		}
+		$randomInt = rand(1, $sum);
+		$sumWeight = 0;
+		foreach( $ads as $ad ) {
+			$sumWeight += $ad->advertisment_weight;
+			if ( $randomInt <= $sumWeight ) {
+				$id = $ad->advertisment_id;
+				break;
+			}
+		}
+		return new Advertisment( $id );
 	}
 	
 	/**
@@ -43,6 +89,7 @@ class WAS_Class {
 		$ad = new Advertisment();
 		$ad->setName( $wpdb->escape( $entry['advertisment_name'] ) );
 		$ad->setHtml( $entry['advertisment_code'] );
+		$ad->setWeight( $entry['advertisment_weight'] );
 		$ad->setActive( ( isset( $entry['advertisment_active'] ) ) ? true : false );
 		$ad->updateDatabase();
 	}
@@ -58,6 +105,7 @@ class WAS_Class {
 		$ad = new Advertisment( $entry['advertisment_id'] );
 		$ad->setName( $wpdb->escape( $entry['advertisment_name'] ) );
 		$ad->setHtml( $entry['advertisment_code'] );
+		$ad->setWeight( $entry['advertisment_weight'] );
 		$ad->setActive( ( isset( $entry['advertisment_active'] ) ) ? true : false );
 		$ad->updateDatabase();
 	}
