a179796ce6371ad6cd6803374a73271ed0e184f0
kdizas@gmail.com <kdizas@gmail.com>
2010-11-10 16:34:39 +0000

    now also removes the soon to be used was_show_per_page option

--- uninstall.php
+++ uninstall.php
@@ -1,15 +1,16 @@
 <?php
 
-if( !defined( 'ABSPATH') && !defined('WP_UNINSTALL_PLUGIN') )
+if( ! defined( 'ABSPATH' ) && ! defined( 'WP_UNINSTALL_PLUGIN' ) )
 	exit();
 
-if ( function_exists('register_uninstall_hook') )
+if ( function_exists( 'register_uninstall_hook' ) )
 	register_uninstall_hook( __FILE__, 'was_uninstall_hook' );
 
 function my_uninstall_hook() {
 	
-	delete_option('was_db_version');
-	
+	delete_option( 'was_db_version' );
+	delete_option( 'was_show_per_page' );
+
 }
 
 ?>
